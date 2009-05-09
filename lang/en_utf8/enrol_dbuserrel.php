3a2c26b53e6bbefc178adc56c1af1deeb309d37f
Myles Carrick <myles@catalyst.net.nz>
2009-05-09 20:13:56 +1000

    lang: moved to auto location

--- lang/en_utf8/enrol_dbuserrel.php
+++ lang/en_utf8/enrol_dbuserrel.php
@@ -1,6 +1,4 @@
-<?PHP 
-
-
+<?php
 $string['dbtype'] = 'Database type';
 $string['dbhost'] = 'Server IP name or number';
 $string['dbuser'] = 'Server user';
@@ -10,7 +8,7 @@ $string['dbtable'] = 'Database table';
 $string['useenroldatabase'] = 'Use the same settings for database connection as the Database enrolment plugin is using (You will still have to specify table name)';
 $string['useauthdb'] = 'Use the same settings for database connection as the Database authentication plugin is using (You will still have to specify table name)';
 $string['description'] = 'You can use a external database (of nearly any kind) to control your relationships between users. It is assumed your external database contains a field containing two user IDs, and a Role ID.  These are compared against fields that you choose in the local user and role tables';
-$string['enrolname'] = 'External Database (User relationships)'; 
+$string['enrolname'] = 'External Database (User relationships)';
 $string['localrolefield'] = 'The name of the field in the roles table that we are using to match entries in the remote database (eg shortname).';
 $string['localsubjectuserfield'] = 'The name of the field in the user table that we are using to match entries in the remote database (eg idnumber). for the <i>subject</i> role assignment';
 $string['localobjectuserfield'] = 'The name of the field in the user table that we are using to match entries in the remote database (eg idnumber). for the <i>object</i> role assignment';
@@ -19,5 +17,4 @@ $string['remoterolefield'] = 'The name of the field in the remote table that we 
 $string['remotesubjectuserfield'] = 'The name of the field in the remote table that we are using to match entries in the user table for the <i>subject</i> role assignment';
 $string['remoteobjectuserfield'] = 'The name of the field in the remote table that we are using to match entries in the user table for the <i>object</i> role assignment';
 $string['server_settings'] = 'External Database Server Settings';
-
 ?>
